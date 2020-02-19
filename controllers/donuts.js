928c10f6b40ab04733ba6a37f1d046909f986c00
John Jacobs <john.a.jacobs1973@gmail.com>
2020-02-19 09:13:10 -0500

    clean up code

--- controllers/donuts.js
+++ controllers/donuts.js
@@ -1,46 +1,33 @@
 let express = require('express')
 let donutRouter = express.Router()
 
-//= =====================
-// REQUIREMENTS
-//= =====================
-// require the Donut model
 const Donut = require('../models/Donut')
 
-//= =====================
-// INDEX
-//= =====================
-// Create a GET index route "/" that sends all Donuts to donuts/index.hbs view
 donutRouter.get('/', (req, res) => {
     Donut.find().then((donuts) => {
         res.render('donuts/index', { donuts })
     })
 })
 
-//= =====================
-// NEW
-//= =====================
-// Create a GET new route "/new" that renders the new.hbs form
 
 donutRouter.get('/new', (req, res) => {
     res.render('donuts/new')
 })
 
-//= =====================
-// SHOW
-//= =====================
-// Create a GET show route "/:id" that renders a single Donut's show page
+donutRouter.get('/random', (req,res) => {
+    Donut.find().then((donuts) => {
+        let i = Math.floor(Math.random() * donuts.length)
+        let donut = donuts[i]
+        res.render('donuts/show', { donut })
+    })
+})
+
 donutRouter.get('/:id', (req, res) => {
     Donut.findById(req.params.id).then(donut => {
         res.render('donuts/show', { donut })
     })
 })
 
-//= =====================
-// CREATE
-//= =====================
-// Create a POST index route "/" that creates a new Donut
-// and upon success redirects back to the index page "/"
 
 donutRouter.post('/', (req, res) => {
     Donut.create(req.body).then((newDonut) => {
@@ -48,11 +35,8 @@ donutRouter.post('/', (req, res) => {
     })
 })
 
-//= =====================
-// EDIT
-//= =====================
-// Create a GET edit route "/:id/edit" that renders the edit.hbs page and
-// sends that a Donut's data to it
+
+
 
 donutRouter.get('/:id/edit', (req, res) => {
     Donut.findById(req.params.id).then(donut => {
@@ -60,29 +44,26 @@ donutRouter.get('/:id/edit', (req, res) => {
     })
 })
 
-//= =====================
-// UPDATE
-//= =====================
-// Create a PUT update route "/:id" that updates the Donut and
-// redirects back to the SHOW PAGE (not index)
 
 donutRouter.put('/:id', (req, res) => {
     Donut.findByIdAndUpdate(req.params.id, req.body).then((updatedDonut) => {
         res.redirect(`/${updatedDonut._id}`)
     })
 })
-//= =====================
-// DELETE
-//= =====================
-// Create a DELETE delete route "/:id" that deletes the Donut and
-// redirects back to index page "/"
+
 donutRouter.delete('/:id', (req,res) => {
     Donut.findByIdAndRemove(req.params.id).then(()=> {
         res.redirect('/')
     })
 })
-//= =====================
-// EXPORTS
-//= =====================
-// export the controller with module.exports
+
+donutRouter.put('/:id/buy', (req,res) => {
+    Donut.findById(req.params.id).then((donut) => {
+            donut.qty -= 1
+            return donut.save()
+    }).then((donut)=> {
+        res.redirect('/'+ donut.id)
+    })
+})
+
 module.exports = donutRouter
--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,9 @@
-// requirements: import mongoose
+
 const mongoose = require('mongoose')
 
-// Connect to a local database called "the-donut-shop"
-// When it connects, then console.log "Connected to MongoDB"
 
 mongoose.connect("mongodb://localhost/the-donut-shop", { useNewUrlParser: true,useUnifiedTopology: true }).then(() => {
     console.log("MONGODB is now connected")
 })
 
-// export your mongoose connection
 module.exports = mongoose
